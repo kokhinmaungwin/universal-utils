13a5962bd8d7aa363cc71c9919358ad99328160f
kokhinmaungwin <231964401+kokhinmaungwin@users.noreply.github.com>
2026-01-05 02:05:19 +0630

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 const resolve = require('@rollup/plugin-node-resolve').default;
 const commonjs = require('@rollup/plugin-commonjs');
-const terser = require('@rollup/plugin-terser').terser;  
+const { terser } = require('@rollup/plugin-terser');  
 
 module.exports = {
   input: 'index.js',
