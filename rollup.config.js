6060ac048a711f6b4a477ac2f2dd783c715fad16
kokhinmaungwin <231964401+kokhinmaungwin@users.noreply.github.com>
2026-01-05 01:55:26 +0630

    Update rollup.config.js and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 const resolve = require('@rollup/plugin-node-resolve').default;
 const commonjs = require('@rollup/plugin-commonjs');
-const { terser } = require('@rollup/plugin-terser');
+const terser = require('@rollup/plugin-terser').terser;  
 
 module.exports = {
   input: 'index.js',
   output: {
     file: 'dist/bundle.min.js',
-    format: 'cjs',  // CommonJS format
+    format: 'cjs',
     sourcemap: true,
   },
   plugins: [
